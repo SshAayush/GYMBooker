95beeed4b9ef57d46410e07e89268d7f5a29eebb
AshishSatyal <ashishsatyal4@gmail.com>
2023-05-08 21:59:56 +0545

    dashboard javascript added

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,1 +1,32 @@
 "use strict";
+
+//selecting buttons
+const dashboardButton = document.querySelector("#dashboard");
+const classesButton = document.querySelector("#classes");
+const activityButton = document.querySelector("#activity");
+
+// selecting containers
+const dashContainer = document.querySelector("#dashboard-container");
+const classesContainer = document.querySelector("#class-container");
+const activityContainer = document.querySelector("#activity-container");
+
+// removing every container
+
+dashboardButton.addEventListener("click", () => {
+  removeContainer();
+  dashContainer.style.display = "block";
+});
+classesButton.addEventListener("click", () => {
+  removeContainer();
+  classesContainer.style.display = "block";
+});
+activityButton.addEventListener("click", () => {
+  removeContainer();
+  activityContainer.style.display = "flex";
+});
+
+function removeContainer() {
+  dashContainer.style.display = "none";
+  classesContainer.style.display = "none";
+  activityContainer.style.display = "none";
+}
